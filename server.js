c564b4c11f17f0dd080ae844b492ad925b990b3a
00014889 <62424147+frrtshbyv@users.noreply.github.com>
2023-04-04 11:28:47 -0400

    Update server.js

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 app.listen(
   PORT,
