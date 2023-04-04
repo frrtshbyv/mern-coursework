99ae2751a1bb4d4b706051f0cb6ddabb2c28187b
00014889 <62424147+frrtshbyv@users.noreply.github.com>
2023-04-04 11:10:47 -0400

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,6 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json()); // It parses incoming requests with JSON payloads and is based on body-parser.
-app.use(cors()); 
 
 
 
@@ -32,7 +31,6 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('frontend/build'));
 
   app.get('/', (req, res) => {
-    res.setHeader("Access-Control-Allow-Credentials", "true");
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
   });
 }
